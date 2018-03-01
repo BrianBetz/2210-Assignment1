9f20e25f292245fbd9aca56528912ef7361df0e7
Brian Betz <betzbri@auburn.edu>
2018-02-28 21:32:26 -0600

    Add files via upload

---
+++ SelectorTest1.java
@@ -0,0 +1,14 @@
+public class SelectorTest1 extends Selector{
+
+   public static void main(String[] args) {
+   
+      Selector s = new Selector();
+   
+      int[] a = {8, 7, 6, 5, 4};
+   
+      s.kmax(a, 5);
+   
+   
+   }
+
+}
